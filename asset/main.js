91477fb7f76a28b7c59252ee3e4dc18c103b3d6e
federico697 <federico697697@gmail.com>
2022-10-18 14:28:40 +0200

    fine esercizio

--- asset/main.js
+++ asset/main.js
@@ -1,2 +1,19 @@
-let = 10
-console.log(let)
+// chiedo il nome
+
+let person = prompt("Please enter your name");
+
+// chiedo il cognome
+
+let lastname = prompt("Please enter your last name");
+
+
+// chiedo il colore preferito
+
+let favcol = prompt("Please enter your favorite color");
+
+// inserisco un numero costante
+const numero = 10
+// console.log (numero)
+
+// mostro all'utente la password
+document.getElementById("password").innerHTML ="la tua password insicurissima è: " + person +lastname+favcol+numero ;
